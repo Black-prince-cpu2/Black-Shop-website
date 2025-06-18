77102490b2f5b651779d3a898a5eca4347873734
somtomichayah@gmail.com <michayahsomto@gmail.com>
2025-06-18 09:53:31 +0100

    'Added_sample_images_and_checkboxes'

---
+++ App.js
@@ -0,0 +1,11 @@
+x=document.querySelectorAll('.dropdown');
+x[0].checked=true
+
+while(true){
+    for (y of x){ 
+        if (y.checked == true){
+            y.parentElement.style.backgroundColor = 'red'
+        }
+    
+}
+}
